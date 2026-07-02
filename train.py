45323ac9643074feada9e1609f36eef10fd3f667
Abdullah Nazhat Abdullah <abdullah.nazhat@gmail.com>
2026-07-02 22:23:26 +0300

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import torch
 from torch import nn 
 from torch.utils.data import DataLoader 
 from torchvision import datasets 
-from torchvision.transforms import ToTensor, Normalize, RandomCrop, RandomHorizontalFlip, RandomRotation, Compose 
+from torchvision.transforms import ToTensor, Normalize, Compose 
 from ninformer import NiNformer
 
 transform = Compose([
